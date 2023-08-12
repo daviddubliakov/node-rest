75cd3ddc6a326ef2f9fc8599f5a6f6e1c3bb3d4f
david.dub <david.dub@brainstack.team>
2023-08-12 19:40:32 +0300

    adds post request

--- controllers/feed.js
+++ controllers/feed.js
@@ -1,1 +1,19 @@
-exports.getPosts = (req, res, next) => {};
+exports.getPosts = (req, res, next) => {
+  res.status(200).json({
+    posts: [
+      {
+        title: 'first post',
+        content: 'This is the first post!',
+      },
+    ],
+  });
+};
+
+exports.createPost = (req, res, next) => {
+  const { title, content } = req.body;
+
+  res.status(201).json({
+    post: { id: new Date().toISOString(), title, content },
+    message: 'Post created successfully',
+  });
+};
--- routes/feed.js
+++ routes/feed.js
@@ -1,10 +1,13 @@
 const express = require('express');
 
-const feedController = require('../controller/feed');
+const feedController = require('../controllers/feed');
 
 const router = express.Router();
 
 // GET /feed/posts
-router.get('/posts', feedController.getPost);
+router.get('/posts', feedController.getPosts);
+
+// POST /feed/posts
+router.post('/posts', feedController.createPost);
 
 module.exports = router;
