005918b0af2ddd77d6724aafa891297a591fe7d0
david.dub <david.dub@brainstack.team>
2023-08-23 21:43:45 +0300

    stores posts in database

--- controllers/feed.js
+++ controllers/feed.js
@@ -1,5 +1,7 @@
 const { validationResult } = require('express-validator');
 
+const Post = require('../models/post');
+
 exports.getPosts = (req, res, next) => {
   res.status(200).json({
     posts: [
@@ -21,25 +23,27 @@ exports.createPost = (req, res, next) => {
   const errors = validationResult(req);
 
   if (!errors.isEmpty()) {
-    return res
-      .status(422)
-      .json({
-        message: 'Validation failed, entered data is incorrect.',
-        errors: errors.array(),
-      });
+    return res.status(422).json({
+      message: 'Validation failed, entered data is incorrect.',
+      errors: errors.array(),
+    });
   }
 
   const { title, content } = req.body;
-
-  res.status(201).json({
-    post: {
-      _id: new Date().toISOString(),
-      title,
-      content,
-      creator: { name: 'Davyd' },
-      createdAt: new Date(),
-    },
-
-    message: 'Post created successfully',
+  const post = new Post({
+    title,
+    content,
+    imageUrl: 'images/duck.jpeg',
+    creator: { name: 'Davyd' },
   });
+
+  post
+    .save()
+    .then((result) => {
+      res.status(201).json({
+        post: result,
+        message: 'Post created successfully',
+      });
+    })
+    .catch((err) => console.log(err));
 };
