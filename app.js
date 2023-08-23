3d3b0bee8bf586cfe3c6f9068396ef6a337dc8ad
david.dub <david.dub@brainstack.team>
2023-08-23 21:54:51 +0300

    addds static images and error handling

--- app.js
+++ app.js
@@ -1,12 +1,14 @@
 const express = require('express');
 const bodyParser = require('body-parser');
 const mongoose = require('mongoose');
+const path = require('path');
 
 const feedRoutes = require('./routes/feed');
 
 const app = express();
 
 app.use(bodyParser.json());
+app.use('/images', express.static(path.join(__dirname, 'images')));
 
 app.use((_req, res, next) => {
   res.setHeader('Access-Control-Allow-Origin', '*');
@@ -19,6 +21,14 @@ app.use((_req, res, next) => {
 });
 
 app.use('/feed', feedRoutes);
+
+app.use((error, req, res, next) => {
+  console.log(error);
+  const status = error.statusCode || 500;
+  const message = error.message;
+
+  res.status(status).json({ message });
+});
 
 mongoose
   .connect(
