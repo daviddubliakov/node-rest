1e7add29be39f26de59ea2fd747511c95cc43fdb
d.dubliakov <d.dubliakov@autodoc.eu>
2023-12-29 23:12:20 +0200

    adds updating posts

--- routes/feed.js
+++ routes/feed.js
@@ -8,13 +8,21 @@ const router = express.Router();
 // GET /feed/posts
 router.get('/posts', feedController.getPosts);
 
-// POST /feed/posts
+// POST /feed/post
 router.post(
   '/post',
-  [body('title').trim().isLength(5), body('content').trim().isLength(5)],
+  [body('title').trim().isLength({ min: 5 }), body('content').trim().isLength({ min: 5 })],
   feedController.createPost,
 );
 
+// GET /feed/post/:postId
 router.get('/post/:postId', feedController.getPost);
+
+// PUT /feed/post/:postId
+router.put(
+  '/post/:postId',
+  [body('title').trim().isLength({ min: 5 }), body('content').trim().isLength({ min: 5 })],
+  feedController.updatePost
+);
 
 module.exports = router;
