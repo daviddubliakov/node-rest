b757fc06813cd97770cbd9c6c715740bdd0fc214
d.dubliakov <d.dubliakov@autodoc.eu>
2023-12-30 12:24:15 +0200

    adding pagination

--- controllers/feed.js
+++ controllers/feed.js
@@ -4,11 +4,24 @@ const path = require('path');
 
 const Post = require('../models/post');
 
-exports.getPosts = (_req, res, next) => {
+exports.getPosts = (req, res, next) => {
+  const currentPage = req.query.page || 1;
+  const perPage = 2;
+  let totalItems;
+
   Post.find()
+    .countDocuments()
+    .then((count) => {
+      totalItems = count;
+
+      return Post.find()
+        .skip((currentPage - 1) * perPage)
+        .limit(perPage);
+    })
     .then((posts) => {
       res.status(200).json({
         posts,
+        totalItems: totalItems,
         message: 'Posts are successfully fetched',
       });
     })
