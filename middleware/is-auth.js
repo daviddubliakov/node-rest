0f5f6c003091c57295c15a491ccd3a7c0feab6d0
d.dubliakov <d.dubliakov@autodoc.eu>
2024-01-04 14:46:12 +0200

    using and validation the token

---
+++ middleware/is-auth.js
@@ -0,0 +1,30 @@
+const jwt = require('jsonwebtoken');
+
+module.exports = (req, res, next) => {
+  const authHeader = req.get('Authorization');
+
+  if (!authHeader) {
+    const error = new Error('Not autheticated.');
+    error.statusCode = 301;
+    throw error;
+  }
+
+  const token = authHeader.split(' ')[1];
+  let decodedToken;
+
+  try {
+    decodedToken = jwt.verify(token, 'secret');
+  } catch (error) {
+    error.statusCode = 500;
+    throw error;
+  }
+
+  if (!decodedToken) {
+    const error = new Error('Not autheticated.');
+    error.statusCode = 301;
+    throw error;
+  }
+
+  req.userId = decodedToken.userId;
+  next();
+};
