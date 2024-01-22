9cba3cf850c99430454f3d8aacbb482ba4928675
d.dubliakov <d.dubliakov@autodoc.eu>
2024-01-22 17:30:20 +0200

    updating posts

--- controllers/feed.js
+++ controllers/feed.js
@@ -148,6 +148,7 @@ exports.updatePost = (req, res, next) => {
   }
 
   Post.findById(postId)
+    .populate('creator')
     .then((post) => {
       if (!post) {
         const error = new Error('No post found');
@@ -156,7 +157,7 @@ exports.updatePost = (req, res, next) => {
         throw error;
       }
 
-      if (post.creator.toString() !== req.userId) {
+      if (post.creator._id.toString() !== req.userId) {
         const error = new Error('Not authenticated');
         error.statusCode = 403;
         throw error;
@@ -173,6 +174,7 @@ exports.updatePost = (req, res, next) => {
       return post.save();
     })
     .then((result) => {
+      io.getIO().emit('posts', { action: 'update', post: result });
       res.status(200).json({ message: 'Post updated!', post: result });
     })
     .catch((err) => {
